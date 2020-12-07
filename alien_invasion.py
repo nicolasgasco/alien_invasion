ad207362bb203f14aaaa4df15b52cd1fe8287e3b
nicolasgasco <nicolas.gasco92@gmail.com>
2020-12-07 14:58:42 +0100

    The fleet of aliens now bounces left and right while going downwards

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bulltets()
+            self._update_aliens()
             self._update_screen()
 
     def _update_bulltets(self):
@@ -140,6 +141,26 @@ class AlienInvasion:
         # Make most recently drawn screen visible.
         pygame.display.flip()
 
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update the positions of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
         
 
