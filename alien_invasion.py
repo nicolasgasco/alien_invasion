3e11ba3740b3c85369db498f444adb5ea2e12058
nicolasgasco <nicolas.gasco92@gmail.com>
2020-12-08 11:02:29 +0100

    Added three ships to represent lives

--- alien_invasion.py
+++ alien_invasion.py
@@ -77,6 +77,10 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
+
 
     def _check_events(self):
         """ Respond to keypresses and mouse events."""
@@ -102,6 +106,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -236,8 +242,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            # Decrement ships_left
+            # Decrement ships_left, and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
--- game_stats.py
+++ game_stats.py
@@ -17,3 +17,4 @@ class GameStats:
         """Initialize statistics that cna change during the game"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
